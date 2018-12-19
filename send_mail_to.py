9d6bab9eaffcbd95f62ad77185e27a8b12e708f0
lamplusoka <43769806+lamplusoka@users.noreply.github.com>
2018-12-20 08:24:56 +0900

    feat:

---
+++ send_mail_to.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import smtplib
+from email.mime.text import MIMEText
+
+from_address = 'eye.drops.everyday@gmail.com'
+#to_address   = 'eye.drops.everyday@gmail.com'
+
+def send_mail_to(pass_from_URL, to_address, message):
+    password      = str(pass_from_URL)
+    msg = MIMEText(message)
+    msg['subject'] = 'Eye Drops Report'
+    #smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpobj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    smtpobj.ehlo()
+    smtpobj.starttls
+    smtpobj.ehlo
+    smtpobj.login(from_address, password)
+    smtpobj.sendmail(from_address, to_address, msg.as_string())
+    smtpobj.quit()
---
+++ verify_eye_drops.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import ast
+import cgi #CGIのフォーム入力を受け付けてくれるモジュール
+import datetime
+import json
+import linecache
+import cgitb
+import send_mail_to as mailTo
+
+cgitb.enable() 
+form = cgi.FieldStorage() #フォーム入力(URLからの入力)が入る
+name_file_json  = 'output.json'
+path_file_json  = "./" + name_file_json
+
+print("content-Type: text/html\n\n")
+print('<h1>Eye Drops</h1>')
+
+today   = {'day': str(datetime.date.today())}
+#print('日本語')
+
+read_date_last = linecache.getline(name_file_json, 2) #jsonファイルの2行目を取得
+change_date_last_to_dict = ast.literal_eval(read_date_last)
+#print(change_date_last_to_dict['day'])
+
+if today == change_date_last_to_dict :
+    message_done = 'Already done, ' + today['day'] + '.'
+    print('<p>' + message_done + '</p>')
+    mailTo.send_mail_to(form['password'].value, form['toAddress'].value, message_done)
+else:
+    message_good = 'Good work! Good remain! ' + today['day'] + '.'
+    print('<p>' + message_good + '</p>')
+    mailTo.send_mail_to(form['password'].value, form['toAddress'].value, message_good)
+
+with open(name_file_json,'wb') as f:
+    f.seek(0, 2)
+    #if f.tell() == 0:
+    f.write('[\n'.encode())
+    f.write(json.dumps(today,  ensure_ascii=False).encode())
+    f.write('\n]'.encode())
